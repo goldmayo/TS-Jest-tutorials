0c674bc529bd89e4e5cf141ac7462ed0e4d6e3d3
HyunSJ <dudtod1596@gmail.com>
2022-04-06 23:55:59 +0900

    add basic/

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "json"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)?$": "ts-jest",
   },
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src/"],
+
+  testMatch: [
+    "<rootDir>/**/*.test.(js|jsx|ts|tsx)",
+    "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
+  ],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
   clearMocks: true,
   collectCoverage: false,
   coverageDirectory: "coverage",
--- src/test/add.test.ts
+++
@@ -1,5 +0,0 @@
-const add_test = require("../add");
-
-test("add 1 + 2 to equal 3", () => {
-  expect(add_test(1, 2)).toBe(3);
-});
